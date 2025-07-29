b8718576b7cdc85632ccd0e1a3cd5d413d8d1996
Shahir-habib <shahirhabib4@gmail.com>
2025-07-29 22:42:43 +0530

    some minor changes

--- server/index.js
+++ server/index.js
@@ -2,30 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRouter = require("../routes/auth/auth-routes");
-const adminProductsRouter = require("../routes/admin/products-routes");
-const adminOrderRouter = require("../routes/admin/order-routes");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
 
-const shopProductsRouter = require("../routes/shop/products-routes");
-const shopCartRouter = require("../routes/shop/cart-routes");
-const shopAddressRouter = require("../routes/shop/address-routes");
-const shopOrderRouter = require("../routes/shop/order-routes");
-const shopSearchRouter = require("../routes/shop/search-routes");
-const shopReviewRouter = require("../routes/shop/review-routes");
-const commonFeatureRouter = require("../routes/common/feature-routes");
-
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
+const commonFeatureRouter = require("./routes/common/feature-routes");
 require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connection successful"))
   .catch((error) => console.log(error));
    
-const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      /\.vercel\.app$/ // Allow all subdomains from vercel.app
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -37,7 +40,6 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
